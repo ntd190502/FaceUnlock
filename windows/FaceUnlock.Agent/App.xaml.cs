7c799a1d642d9297a0d2f557bee51e92b20c29fc
Nguyễn Đạt <ntd190502@gmail.com>
2026-08-21 01:26:21 +0700

    Disambiguate WPF Application type

--- windows/FaceUnlock.Agent/App.xaml.cs
+++ windows/FaceUnlock.Agent/App.xaml.cs
@@ -1,7 +1,6 @@
-using System.Windows;
 namespace FaceUnlock.Agent;
-public partial class App : Application {
+public partial class App : System.Windows.Application {
  private InteractiveBridge? _bridge;
- protected override void OnStartup(StartupEventArgs e){base.OnStartup(e);_bridge=new InteractiveBridge();}
- protected override void OnExit(ExitEventArgs e){_bridge?.Dispose();base.OnExit(e);}
+ protected override void OnStartup(System.Windows.StartupEventArgs e){base.OnStartup(e);_bridge=new InteractiveBridge();}
+ protected override void OnExit(System.Windows.ExitEventArgs e){_bridge?.Dispose();base.OnExit(e);}
 }
