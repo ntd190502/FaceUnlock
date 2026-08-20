4a60a9085424f054cfc344d83986a58f8bc62907
Nguyễn Đạt <ntd190502@gmail.com>
2026-08-21 01:33:47 +0700

    Wire monitoring settings window

---
+++ windows/FaceUnlock.Agent/MainWindow.Monitoring.cs
@@ -0,0 +1,4 @@
+namespace FaceUnlock.Agent;
+public partial class MainWindow {
+ private void Monitoring_Click(object sender,System.Windows.RoutedEventArgs e){new MonitoringSettingsWindow{Owner=this}.ShowDialog();}
+}
