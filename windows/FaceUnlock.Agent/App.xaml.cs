020eace3b9dc3a1fa751625ea68fe7601f8e6477
Nguyễn Đạt <ntd190502@gmail.com>
2026-08-21 01:18:16 +0700

    Start interactive remote bridge with Agent

--- windows/FaceUnlock.Agent/App.xaml.cs
+++ windows/FaceUnlock.Agent/App.xaml.cs
@@ -1,1 +1,7 @@
-using System.Windows; namespace FaceUnlock.Agent; public partial class App:Application{}
+using System.Windows;
+namespace FaceUnlock.Agent;
+public partial class App : Application {
+ private InteractiveBridge? _bridge;
+ protected override void OnStartup(StartupEventArgs e){base.OnStartup(e);_bridge=new InteractiveBridge();}
+ protected override void OnExit(ExitEventArgs e){_bridge?.Dispose();base.OnExit(e);}
+}
