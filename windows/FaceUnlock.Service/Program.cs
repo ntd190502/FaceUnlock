1c8f5159403c2e61996dd2e8730760c5f6a2fe08
Nguyễn Đạt <ntd190502@gmail.com>
2026-08-21 21:05:33 +0700

    Persist remote diagnostics to service.log

--- windows/FaceUnlock.Service/Program.cs
+++ windows/FaceUnlock.Service/Program.cs
@@ -2,6 +2,10 @@ using FaceUnlock.Service;
 
 Host.CreateDefaultBuilder(args)
     .UseWindowsService(options => options.ServiceName = "FaceUnlock Service")
+    .ConfigureLogging(logging =>
+    {
+        logging.AddProvider(new ServiceFileLoggerProvider());
+    })
     .ConfigureServices(services =>
     {
         services.AddHostedService(provider => new UnlockWorker(provider.GetRequiredService<ILogger<UnlockWorker>>()));
