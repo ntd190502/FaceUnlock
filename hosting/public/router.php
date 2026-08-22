aa758ce151f6b97cdd7bc41b8e468e670ead3706
Nguyễn Đạt <ntd190502@gmail.com>
2026-08-22 11:32:09 +0700

    Route file transfer API in development server

--- hosting/public/router.php
+++ hosting/public/router.php
@@ -1,6 +1,2 @@
 <?php
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$file = __DIR__.$uri;
-if ($uri !== '/' && is_file($file)) return false;
-if (str_starts_with((string)$uri, '/v1/control/')) require __DIR__.'/control.php';
-else require __DIR__.'/index.php';
+$uri=parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);$file=__DIR__.$uri;if($uri!=='/'&&is_file($file))return false;if(str_starts_with((string)$uri,'/v1/control/'))require __DIR__.'/control.php';elseif(str_starts_with((string)$uri,'/v1/files/'))require __DIR__.'/files.php';else require __DIR__.'/index.php';
